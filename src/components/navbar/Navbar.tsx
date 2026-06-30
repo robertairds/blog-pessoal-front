df5ece57719ddb306e84826073efd889e9901049
Roberta Rodrigues <roberta673@gmail.com>
2026-06-30 18:21:42 -0300

    Atualização

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,39 +1,39 @@
 import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
-  // Insira o Código aqui!
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
-const { handleLogout } = useContext(AuthContext);
+    const { handleLogout } = useContext(AuthContext)
 
-function logout() {
+    function logout(){
+        
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
 
-    handleLogout();
-    alert('O Usuário foi desconectado com sucesso!');
-    navigate('/');
-}
-
-  return (
-    <>
-      <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
-
-        <div className="container flex justify-between text-lg mx-8">
-          <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-
-          <div className='flex gap-4'>
-            Postagens
-            Temas
-            Cadastrar tema
-            Perfil
-            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+                
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+                    
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
